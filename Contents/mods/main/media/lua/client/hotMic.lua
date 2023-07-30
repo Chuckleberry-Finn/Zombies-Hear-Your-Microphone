c5c0bbecc14db2c7ceaae1f18653e641c8f2febb
Chuckleberry Finn <6071161+Chuckleberry-Finn@users.noreply.github.com>
2023-07-29 20:05:12 -0400

    remove ; and replace getPlayer calls

--- Contents/mods/main/media/lua/client/hotMic.lua
+++ Contents/mods/main/media/lua/client/hotMic.lua
@@ -8,7 +8,7 @@ function hotMic.onPlayerUpdate(playerObj)
     getCore():setTestingMicrophone(true)
 
 
-	local traitsMultiplier = 0;
+	local traitsMultiplier = 0
     local influence = SandboxVars.ZombiesHearYourMicrophone.traitsinfluence
 	local minimalr = SandboxVars.ZombiesHearYourMicrophone.minimalradius
 	local sneakr = SandboxVars.ZombiesHearYourMicrophone.sneakreduce
@@ -17,50 +17,48 @@ function hotMic.onPlayerUpdate(playerObj)
         traitsMultiplier = 0
     else
         -- bad traits 2 or 4 (both)
-        traitsMultiplier = traitsMultiplier + ((influence == 2 or influence == 4) and getPlayer():HasTrait("Conspicuous") and 1 or 0);
-        traitsMultiplier = traitsMultiplier + ((influence == 2 or influence == 4) and getPlayer():HasTrait("Clumsy") and 0.2 or 0);
+        traitsMultiplier = traitsMultiplier + ((influence == 2 or influence == 4) and playerObj:HasTrait("Conspicuous") and 1 or 0)
+        traitsMultiplier = traitsMultiplier + ((influence == 2 or influence == 4) and playerObj:HasTrait("Clumsy") and 0.2 or 0)
 
         -- good traits 3 or 4 (both)
-        traitsMultiplier = traitsMultiplier - ((influence == 3 or influence == 4) and getPlayer():HasTrait("Graceful") and 0.6 or 0);
-        traitsMultiplier = traitsMultiplier - ((influence == 3 or influence == 4) and getPlayer():HasTrait("Inconspicuous") and 0.5 or 0);
+        traitsMultiplier = traitsMultiplier - ((influence == 3 or influence == 4) and playerObj:HasTrait("Graceful") and 0.6 or 0)
+        traitsMultiplier = traitsMultiplier - ((influence == 3 or influence == 4) and playerObj:HasTrait("Inconspicuous") and 0.5 or 0)
 
         if traitsMultiplier < -0.9 then
-            traitsMultiplier = -0.9;
+            traitsMultiplier = -0.9
         end
     end
 
-    local factor = (1 + traitsMultiplier) * SandboxVars.ZombiesHearYourMicrophone.multiplier;
+    local factor = (1 + traitsMultiplier) * SandboxVars.ZombiesHearYourMicrophone.multiplier
     if influence == 3 or influence == 4 then
-        factor = factor * DISCOUNT_VALUES[1 + getPlayer():getPerkLevel(Perks.Lightfoot)] * DISCOUNT_VALUES[1 + getPlayer():getPerkLevel(Perks.Sneak)];
+        factor = factor * DISCOUNT_VALUES[1 + playerObj:getPerkLevel(Perks.Lightfoot)] * DISCOUNT_VALUES[1 + playerObj:getPerkLevel(Perks.Sneak)]
     end
 	
-    if getPlayer():isSneaking() then
-        factor = factor * sneakr;
-    end
+    if playerObj:isSneaking() then factor = factor * sneakr end
 
-	if factor < minimalr then
-		factor = minimalr;
-	end
+	if factor < minimalr then factor = minimalr end
 
-    local volume =  math.min(10, math.max(0, getCore():getMicVolumeIndicator())) * factor
-    local isErr = getCore():getMicVolumeError()
+    local core = getCore()
+    local volume =  math.min(10, math.max(0, core:getMicVolumeIndicator())) * factor
+    local isErr = core:getMicVolumeError()
     if isErr then return end
 
-    local isPTT = getCore():getOptionVoiceMode()==1
-    local isPTTKeyDown = GameKeyboard.isKeyDown(getCore():getKey("Enable voice transmit"))
+    local isPTT = core:getOptionVoiceMode()==1
+    local isPTTKeyDown = GameKeyboard.isKeyDown(core:getKey("Enable voice transmit"))
     if isPTT and not isPTTKeyDown and SandboxVars.ZombiesHearYourMicrophone.respectEnableVOIP then return end
 
-    local serverVOIPEnable = getCore():getServerVOIPEnable()
+    local serverVOIPEnable = core:getServerVOIPEnable()
     if not serverVOIPEnable and SandboxVars.ZombiesHearYourMicrophone.respectEnableVOIP then return end
 
-    local voiceEnabled = getCore():getOptionVoiceEnable()
+    local voiceEnabled = core:getOptionVoiceEnable()
     if not voiceEnabled and SandboxVars.ZombiesHearYourMicrophone.respectEnableVOIP then return end
 
     AddWorldSound(playerObj, volume, volume)
 
     if getDebug() then
-        if circle then getWorldMarkers():removeGridSquareMarker(circle) end
-        circle = getWorldMarkers():addGridSquareMarker("circle_center", "circle_only_highlight", playerObj:getSquare(), 1, 1, 1, true, volume)
+        local worldMarkers = getWorldMarkers()
+        if circle then worldMarkers:removeGridSquareMarker(circle) end
+        circle = worldMarkers:addGridSquareMarker("circle_center", "circle_only_highlight", playerObj:getSquare(), 1, 1, 1, true, volume)
     end
 
 end
