71710c596d43281b7e628697b1cd8c682abfa3c1
Chuckleberry Finn <6071161+Chuckleberry-Finn@users.noreply.github.com>
2023-11-09 20:32:21 -0500

    Update hotMic_eventAdd.lua

--- Contents/mods/main/media/lua/client/hotMic_eventAdd.lua
+++ Contents/mods/main/media/lua/client/hotMic_eventAdd.lua
@@ -1,2 +1,6 @@
 local zombiesCanHearYou = require("hotMic")
 if zombiesCanHearYou then Events.OnPlayerUpdate.Add(zombiesCanHearYou.onPlayerUpdate) end
+
+local modCountSystem = require "chuckleberryFinnModding_modCountSystem"
+if modCountSystem then modCountSystem.pullAndAddModID()
+else print("ERR: MISSING MOD: `ChuckleberryFinnAlertSystem` (Workshop ID: `3077900375`)") end
